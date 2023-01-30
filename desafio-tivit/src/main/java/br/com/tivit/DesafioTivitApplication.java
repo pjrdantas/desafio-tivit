6015df59098cde3e71602bd51e4b85770809f8f4
pjrdantas <pjr_dantas@hotmail.com>
2023-01-30 18:37:01 -0200

    #Primeiro commit

---
+++ desafio-tivit/src/main/java/br/com/tivit/DesafioTivitApplication.java
@@ -0,0 +1,13 @@
+package br.com.tivit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTivitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTivitApplication.class, args);
+	}
+
+}
---
+++ desafio-tivit/src/test/java/br/com/tivit/DesafioTivitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tivit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTivitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
